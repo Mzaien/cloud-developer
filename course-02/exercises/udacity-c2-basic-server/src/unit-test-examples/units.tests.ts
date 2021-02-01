99b70c6e8d81f0182e00689e135fe6bd89d85bb4
mzaien <s201540830@kfupm.edu.sa>
2021-02-01 10:46:52 +0300

    First exresice and uint testing

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide,concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -38,3 +38,15 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+describe("Concat two strings into one",()=>{
+
+it('Add Apple and gold',()=>{
+  const result=concat('Apple','gold');
+  expect(result).to.equal('Applegold')
+});
+
+it('Add Apple and nothing which throw and error',()=>{
+  
+  expect(()=>{concat('Apple')}).to.throw('Please add two words')
+});
+});
--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
@@ -15,3 +15,10 @@ export const divide = (a: number, b: number) => {
 // it should return one string combining the two strings.
 // it should throw an error if either of the strings are empty.
 // ensure your function is exported.
+
+export const concat =(firstWord:string,secondWord?:string)=>{
+    if (firstWord === undefined || secondWord === undefined){
+        throw new Error('Please add two words')
+    }
+    return firstWord.concat(secondWord);
+}
